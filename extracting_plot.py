44f9868bc93275f7c03ba598d651bf220032d804
danyrw <45208254+danyrw@users.noreply.github.com>
2019-11-09 16:08:18 +0100

    Update extracting_plot.py

--- extracting_plot.py
+++ extracting_plot.py
@@ -3,7 +3,7 @@ import os
 #Put html files in the same directory. In this example "/home/computer/Downloads/test"
 all_htmls=os.listdir("/home/computer/Downloads/test")
 for html_file in all_htmls:
-	html=open(html_file)
+	html=open("/home/computer/Downloads/test/"+html_file)
 	plot=[]
 	file = BeautifulSoup(html, 'html.parser')
 	tag = file.select_one('#Plot').find_parent('h2').find_next_sibling()
@@ -12,6 +12,9 @@ for html_file in all_htmls:
 		tag = tag.find_next_sibling()
 	p = ','.join(plot)
 	film_plot = p.replace('\n','').replace("\'","")
-	print(film_plot)
+	print(html_file, "(Plot)")
+	print("-----------")
+	print(film_plot, "\n")
 
-
+#Make sure you put only html fies in the same (eg: "/home/computer/Downloads/test")
+#YOU CAN RUN THIS SCRIPT IN WHATEVER DIRECTORY
