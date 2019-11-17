60dbd16c20787dd124b0b33bce7686bced19517f
ShokoufehMansouri <56389270+ShokoufehMansouri@users.noreply.github.com>
2019-11-17 23:57:14 +0100

    Update parser_utils.py

--- parser_utils.py
+++ parser_utils.py
@@ -1,8 +1,3 @@
-# i clean some field of table
-def SplitWord(word):
-    for k in range(1,len(word)):
-        if word[k].isupper() and word[k-1] != ' ':
-            word = word[:k] + ' ' + word[k:]
-    return word
+
 
 
